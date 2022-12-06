287eae59fe15b9976f6c1e542e0d11df7c9ff608
3TP2 <kuba.mondzelewski@gmail.com>
2022-12-06 11:30:45 +0100

    Initial commit

---
+++ bank_zad4/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace bank_zad4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            start.Visibility = Visibility.Hidden;
+            nowyUz.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        {
+            start.Visibility = Visibility.Visible;
+            nowyUz.Visibility = Visibility.Hidden;
+        }
+    }
+}
